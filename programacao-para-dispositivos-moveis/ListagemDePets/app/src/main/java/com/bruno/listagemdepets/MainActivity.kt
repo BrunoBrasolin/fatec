b95ae2b5e132af77e305d6f2da61b9c41617677b
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-06-26 12:45:49 -0300

    listagem de pets criada

---
+++ programacao-para-dispositivos-moveis/ListagemDePets/app/src/main/java/com/bruno/listagemdepets/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bruno.listagemdepets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
