a9fe2388ef4529af1329400c1425300ae6115119
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-07-10 23:18:33 -0300

    exemplo fragmento criado

---
+++ programacao-para-dispositivos-moveis/ExemploFragment/app/src/main/java/com/bruno/exemplofragment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bruno.exemplofragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
